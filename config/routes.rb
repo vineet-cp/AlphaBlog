1b436d6055ec0dbbbc057ac50e1c8598fcd01ec8
vineet tiwari <vineetjeccs@gmail.com>
2020-06-23 00:39:33 +0530

    table created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles
   root 'pages#home'
-  get  'about',to : 'pages#about'
+  get 'about', to: 'pages#about'
+  resources :articles, only: [:show, :index]
 end
